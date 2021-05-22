3e33553dd9787cd112644fcbf57afa651110cac0
Davide Nurzia <36786636+noorzeea@users.noreply.github.com>
2021-05-22 18:40:40 +0200

    edited setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 
-from python_tools import __version__
-
 setup(
     name='python_tools',
-    version=__version__,
-
+    version='0.0.1',
     url='https://github.com/noorzeea/python_tools',
     author='Davide Nurzia',
     author_email='davide.nurzia@gmail.com',
-
     py_modules=['python_tools'],
 )
